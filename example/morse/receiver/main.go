68e8f06a98ff48e2414b6cc5bf054cb2ca81ce2a
wrfly <mr.wrfly@gmail.com>
2021-06-13 22:34:41 +0800

    relocate example code, morse only

--- example/morse/receiver/main.go
+++ example/morse/receiver/main.go
@@ -9,17 +9,24 @@ import (
 	"github.com/wrfly/simorse/morse"
 )
 
-func main() {
-	pid := os.Getpid()
-	fmt.Println("PID:", pid)
-
+func receive(index int) {
 	cmdChan := morse.ReceiveSignal(context.Background())
 	for x := range cmdChan {
 		if x != "" {
-			log.Printf("%s", x)
+			log.Printf("#%d received: %s", index, x)
 		}
 		if x == "q" {
 			break
 		}
 	}
+}
+
+func main() {
+	pid := os.Getpid()
+	fmt.Println("PID:", pid)
+
+	go receive(1)
+	go receive(2)
+	go receive(3)
+	receive(4)
 }
