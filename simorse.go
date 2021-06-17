0758e6bfe2b4ba2022e5ab340b26576dcabca3b4
wrfly <mr.wrfly@gmail.com>
2021-06-17 22:34:02 +0800

    feat: simple simorse cli

--- simorse.go
+++ simorse.go
@@ -2,6 +2,11 @@ package simorse
 
 import (
 	"context"
+	"fmt"
+	"log"
+	"net"
+	"os"
+	"strings"
 
 	"github.com/wrfly/simorse/morse"
 )
@@ -15,6 +20,26 @@ func watchSignal() {
 	cmdCh := morse.ReceiveSignal(context.Background())
 	for cmd := range cmdCh {
 		debug("receive cmd: %s\n", cmd)
-		Call(cmd)
+		if !strings.HasPrefix(cmd, "simorse_") {
+			fmt.Printf("unknown simorse cmd %s\n", cmd)
+		}
+		cliPID := strings.Split(cmd, "_")[1]
+		conn, err := net.Dial("unix",
+			fmt.Sprintf("%s/simorse-%s.sock", os.TempDir(), cliPID))
+		if err != nil {
+			log.Printf("dial simorse %s socket err: %s", cliPID, err)
+			continue
+		}
+		// read
+		buff := make([]byte, 1024)
+		n, err := conn.Read(buff)
+		if err != nil {
+			log.Printf("read socket err: %s", err)
+			return
+		}
+		log.Printf("read %s", buff[:n])
+
+		// write
+		conn.Write([]byte("pong"))
 	}
 }
